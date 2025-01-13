a96238dd143db2fb13e2d6ad6bc6c5888123c760
Armaan <armaanj32@gmail.com>
2025-01-14 03:22:52 +0400

    Update functions for users table operations
    - Make functions async for use in FastAPI endpoints
    - Improve error handling for more detail and coverage
    - Implement use of pydantic schema
    - Replace testing print statements with return statements conforming to schema
    - Improve code readability
    - Add docstring to every function

---
+++ app/database/__init__.py
@@ -0,0 +1,1 @@
+from .users import create_user, fetch_user, fetch_id, update_user, delete_user
--- app/database/users.py
+++ app/database/users.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client
 from postgrest.exceptions import APIError
+from app.schema.users import UserDataRequest, UserDataResponse, UserUpdateRequest, GeneralResponse
 
 
 load_dotenv()
@@ -10,82 +11,279 @@ DATABASE_API_KEY = os.getenv('SUPABASE_API_KEY')
 database_client = create_client(DATABASE_URL, DATABASE_API_KEY)
 
 
-# Create new user:
-def create_user(data: dict):
+
+
+async def create_user(data: UserDataRequest) -> GeneralResponse:
+    """
+    Function Overview:
+    Creates a new user based on the provided data.
+
+    Function Logic:
+    1. The function attempts to create a new user with the given request data.
+    2. If successful, it returns a structured response wrapped in the GeneralResponse schema.
+    3. Depending on the error raised:
+        - ValueError: Returns a structured response indicating the given email ID or username is in use by another user.
+        - APIError, Exception or an Unexpected Error:  Raises a RuntimeError with a detailed error message.
+
+    Parameters:
+    request (UserDataRequest): Data required to create a new user.
+
+    Returns:
+    GeneralResponse: A response containing the result of the user creation operation.
+    """
     try:
-        response = database_client.table("users").insert(data).execute()
+        request_dict = data.model_dump()
+        response = (
+            database_client
+            .table("users")
+            .insert(request_dict)
+            .execute()
+            )
+
         if response.data:
-            username = data["username"]
-            print(f"User '{username}' created successfully.")
+            return GeneralResponse(
+                detail = f"User '{data.username}' created successfully.",
+                data = None
+                )
         else:
             raise Exception("Unknown error occurred while trying to create a new user.")
+        
     except APIError as e:
-        print(f"API Error: {e}")
+
+        if (e.json()["code"] == "23505"):
+            error_detail = (
+                e.json()["details"]
+                ).split(" ")
+            error_field = error_detail[1]
+
+            if "email_id" in error_field:
+                raise ValueError(f"User with email ID '{data.email_id}' already exists.")
+            
+            if "username" in error_field:
+                raise ValueError(f"User with username '{data.username}' already exists.")
+            
+            else:
+                raise RuntimeError(f"API Error: {e}") from e
+            
+        else:
+            raise RuntimeError(f"API Error: {e}") from e
+
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}") from e
 
 
-# Fetch all user data:
-def fetch_user(id: int):
+
+
+async def fetch_user(id: int) -> GeneralResponse:
+    """
+    Function Overview:
+    Fetches the data of a user based on the given user ID.
+
+    Function Logic:
+    1. The function attempts to fetch user data for the provided user ID.
+    2. If successful, it returns a structured response wrapped in the GeneralResponse schema.
+    3. Depending on the error raised:
+        - ValueError: Returns a structured response indicating the requested user does not exist.
+        - APIError, Exception or an Unexpected Error:  Raises a RuntimeError with a detailed error message.
+
+    Parameters:
+    id (int): The user ID whose data is to be fetched.
+
+    Returns:
+    GeneralResponse: A response containing the result of the fetch operation and the requested user's data.
+    """
     try:
-        response = database_client.table("users").select("*").eq("user_id", id).execute()
+        response = (
+            database_client
+            .table("users")
+            .select("*")
+            .eq("user_id", id)
+            .execute()
+            )
+        
         if response.data:
-            print(response.data[0])
+            return GeneralResponse(
+                detail = f"Details for user ID '{id}' fetched successfully.", 
+                data = UserDataResponse(**(response.data[0]))
+                )
         elif not response.data:
-            raise Exception(f"User ID '{id}' not found.")
+            raise ValueError()
         else:
             raise Exception("Unknown error occurred while trying to fetch user details.")
+        
     except APIError as e:
-        print(f"API Error: {e}")
+        raise RuntimeError(f"API Error: {e}") from e
+    
+    except ValueError as e:
+        raise ValueError(f"User ID '{id}' not found.") from e
+    
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}") from e
 
 
-# Fetch user ID by username:
-def fetch_user_id(username: str):
+
+
+async def fetch_id(username: str) -> GeneralResponse:
+    """
+    Function Overview:
+    Fetches the user ID based on the provided username.
+
+    Function Logic:
+    1. The function attempts to fetch the user ID for the given username.
+    2. If successful, it returns a structured response wrapped in the GeneralResponse schema.
+    3. Depending on the error raised:
+        - ValueError: Returns a structured response indicating the requested user does not exist.
+        - APIError, Exception or an Unexpected Error:  Raises a RuntimeError with a detailed error message.
+
+    Parameters:
+    username (str): The username for which the user ID is to be fetched.
+
+    Returns:
+    GeneralResponse: A response containing the result of the fetch operation and the user ID associated with the requested username.
+    """
     try:
-        response = database_client.table("users").select("user_id").eq("username", username).execute()
+        response = (
+            database_client
+            .table("users")
+            .select("user_id")
+            .eq("username", username)
+            .execute()
+            )
+
         if response.data:
-            print(response.data[0]["user_id"])
+            return GeneralResponse(
+                detail = f"User ID for username '{username}' fetched successfully.", 
+                data = response.data[0]["user_id"]
+                )
         elif not response.data:
-            raise Exception(f"User with username '{username}' not found.")
+            raise ValueError()
         else:
             raise Exception("Unknown error occurred while trying to fetch user ID.")
+        
     except APIError as e:
-        print(f"API Error: {e}")
+        raise RuntimeError(f"API Error: {e}") from e
+    
+    except ValueError as e:
+        raise ValueError(f"Username '{username}' not found.") from e
+
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}") from e
 
 
-# Update user data:
-def update_user(id: int, field: str, data: str):
+
+
+async def update_user(request: UserUpdateRequest) -> GeneralResponse:
+    """
+    Function Overview:
+    Updates the data for an existing user.
+
+    Function Logic:
+    1. The function attempts to update user data based on the provided request.
+    2. If successful, it returns a structured response wrapped in the GeneralResponse schema.
+    3. Depending on the error raised:
+        - ValueError: Returns a structured response indicating the given email ID or username is in use by another user
+        - APIError, Exception or an Unexpected Error:  Raises a RuntimeError with a detailed error message.
+
+    Parameters:
+    request (UserUpdateRequest): Data to update the existing user.
+
+    Returns:
+    GeneralResponse: A response indicating the outcome of the update operation.
+    """
     try:
-        response = database_client.table("users").update({field: data}).eq("user_id", id).execute()
+        id = request.user_id
+        field = request.field
+        data = request.data
+        response = (
+            database_client
+            .table("users")
+            .update({field: data})
+            .eq("user_id", id)
+            .execute()
+            )
+        
         if response.data:
             if field == "first_name" or field == "last_name" or field == "email_id":
                 field = field.replace('_', ' ')
-            print(f"{field.capitalize()} updated successfully for user ID: {id}.")
+            return GeneralResponse(
+                detail = f"{field.capitalize()} updated successfully for user ID '{id}'.",
+                data = None
+                )
         elif not response.data:
-            raise Exception(f"User ID '{id}' not found.")
+            raise ValueError()
         else:
             raise Exception("Unknown error occurred while trying to update user details.")
+        
     except APIError as e:
-        print(f"API Error: {e}")
+        if (e.json()["code"] == "23505"):
+            error_detail = (
+                e.json()["details"]
+                ).split(" ")
+            error_field = error_detail[1]
+
+            if "email_id" in error_field:
+                raise ValueError(f"User with email ID '{request.data}' already exists.")
+            
+            if "username" in error_field:
+                raise ValueError(f"User with username '{request.data}' already exists.")
+            
+            else:
+                raise RuntimeError(f"API Error: {e}") from e
+            
+        else:
+            raise RuntimeError(f"API Error: {e}") from e
+    
+    except ValueError as e:
+        raise ValueError(f"User ID '{id}' not found.") from e
+
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}") from e
 
 
-# Delete user data:
-def delete_user(id: int):
+
+
+async def delete_user(id: int) -> GeneralResponse:
+    """
+    Function Overview:
+    Deletes the user data for the specified user ID.
+
+    Function Logic:
+    1. The function attempts to delete the user data for the provided ID.
+    2. If successful, it returns a structured response wrapped in the GeneralResponse schema.
+    3. Depending on the error raised:
+        - ValueError: Returns a structured response indicating the requested user does not exist.
+        - APIError, Exception or an Unexpected Error:  Raises a RuntimeError with a detailed error message.
+
+    Parameters:
+    id (int): The user ID whose data is to be deleted.
+
+    Returns:
+    GeneralResponse: A response indicating the outcome of the deletion operation.
+    """
     try:
-        response = database_client.table("users").delete().eq("user_id", id).execute()
+        response = (
+            database_client
+            .table("users")
+            .delete()
+            .eq("user_id", id)
+            .execute()
+            )
+        
         if response.data:
-            print(f"User details deleted successfully for user ID: {id}.")
+            return GeneralResponse(
+                detail = f"User details deleted successfully for user ID '{id}'.",
+                data = None
+                )
         elif not response.data:
-            raise Exception(f"User ID '{id}' not found.")
+            raise ValueError()
         else:
             raise Exception("Unknown error occurred while trying to delete user details.")
+        
     except APIError as e:
-        print(f"API Error: {e}")
+        raise RuntimeError(f"API Error: {e}") from e
+    
+    except ValueError as e:
+        raise ValueError(f"User ID '{id}' not found.") from e
+
     except Exception as e:
-        print(f"Unexpected Error: {e}")
+        raise RuntimeError(f"Unexpected Error: {e}")
